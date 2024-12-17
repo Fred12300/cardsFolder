a4d5f58f8c01d2f6ff5f36f82ffbc732d2eaa4b7
Fred12300 <fredcharpentier2@gmail.com>
2024-12-17 16:40:37 +0100

    debut mise en forme

--- src/Repository/FolderRepository.php
+++ src/Repository/FolderRepository.php
@@ -92,28 +92,4 @@ class FolderRepository extends ServiceEntityRepository
 
         return $reverseMatches;
     }
-    //    /**
-    //     * @return Folder[] Returns an array of Folder objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('f')
-    //            ->andWhere('f.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('f.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Folder
-    //    {
-    //        return $this->createQueryBuilder('f')
-    //            ->andWhere('f.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
